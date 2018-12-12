7605246824d5d66f680bd55fdf40e9504e577da7
CosminaP <cosmina.palade@gmail.com>
2018-12-12 23:25:53 +0200

    Update app.js

--- Dice-Game/mini-game/app.js
+++ Dice-Game/mini-game/app.js
@@ -1,12 +1,3 @@
-/*
-GAME RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-*/
-
 let scores = [0 , 0],
     roundScore = 0,
     activePlayer = 0;
@@ -49,9 +40,6 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
             document.querySelector('#btn-txt').style.display="block";
             document.querySelector('.btn-roll').style.display="none";
             document.querySelector('.btn-hold').style.display="none";
-            // document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
-            // console.log(activePlayer);
-            // activePlayer === 0 ?  document.querySelector('#btn-txt').textContent = 'Player 1 WON!' :  document.querySelector('#btn-txt').textContent = 'Player 2 WON!';
             document.querySelector('#name-'+activePlayer).textContent = "WINNER!";
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-0-panel').classList.remove('active');
@@ -71,8 +59,6 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
     }
 
     function restart(){
@@ -82,8 +68,6 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.querySelector('#current-1').textContent = 0;
         document.getElementById('score-0').textContent = 0;
         document.getElementById('score-1').textContent = 0;
-        // document.querySelector('.player-1-panel').classList.remove('active');
-        // document.querySelector('.player-0-panel').classList.remove('active'); document.querySelector('.dice').style.display = 'none';
         document.querySelector('#btn-txt').style.display="none";
         document.querySelector('.btn-roll').style.display="block";
         document.querySelector('.btn-hold').style.display="block";
