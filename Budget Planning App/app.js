046fa0664d8969aa86d7227f6fa4e4389da22a69
Cosmina Palade <cosmina.palade@gmail.com>
2018-12-15 22:22:08 +0200

    Budget App - getting values from inputs and storing values

--- Budget Planning App/app.js
+++ Budget Planning App/app.js
@@ -1,19 +1,54 @@
 // Module budget Controller
 let budgetController = (function(){ 
-    let val = 22;
-    let sum = (a) => { return a + val};
-    return {
-        publicTest: function(b) {
-           return sum(b); 
+
+    let Expense = function (id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: [],
+            inc: []
         }
     }
+
 })();
 
 //the 2 modules are completely independent 
 // SEPARATION of Concerns meaning each part is separated and are doing the things independently
 //module UI Controller
-let uiController = (function(){ 
-    //Some code
+let UIController = (function(){ 
+    let DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value, 
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function () {
+            return DOMStrings;
+        }
+    }
 })();
 
 
@@ -21,10 +56,40 @@ let uiController = (function(){
 
 //Module Controller
 let controller = (function(budgetCtrl, UICtrl){ 
-   let z =  budgetCtrl.publicTest(4);
-    return {
-        anotherPublic: () => console.log(z)
-    };
-})(budgetController, uiController);
 
-controller.anotherPublic();
+    let EventListeners =  function() {
+        let btn = UIController.getDOMstrings();
+        document.querySelector(btn.inputBtn).addEventListener('click', function(){
+            addItem();
+          });
+        
+          document.addEventListener('keypress', function(event){
+            // console.log(event); 
+            if(event.keyCode == 13 || event.which === 13 ) {
+              addItem();
+            }
+        
+          });
+    }
+
+    let addItem = () => {
+      // 1. Get the field input data
+      let input = UICtrl.getInput();
+      console.log(input);
+      // 2. Add the item to the budget controller
+
+      // 3. Add the item to the URI
+
+      //4. Display the budget to the UI
+   }
+
+   return {
+       init: function() {
+            console.log('Application has started'); 
+            EventListeners();
+       }
+   }
+
+})(budgetController, UIController);
+
+controller.init();
